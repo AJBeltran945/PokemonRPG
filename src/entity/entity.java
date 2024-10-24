de86f7537e1b4367be7357361336674d96a48a22
AJBeltran945 <ajbeltran945@gmail.com>
2024-10-24 17:25:56 +0200

    player movement and animation

--- src/entity/entity.java
+++ src/entity/entity.java
@@ -7,7 +7,7 @@ public class entity {
     public int x, y; // position of the entity
     public int speed; // speed of the entity
 
-    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2; // images for the entity
+    public BufferedImage up1, up2, up3, up4, down1, down2, down3, down4, left1, left2, left3, left4, right1, right2, right3, right4; // images for the entity
     public String direction; // direction of the entity
 
     public int spriteCounter = 0; // sprite count
--- src/entity/player.java
+++ src/entity/player.java
@@ -31,12 +31,20 @@ public class player extends entity {
         try {
             up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_up_1.png")));
             up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_up_2.png")));
+            up3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_up_3.png")));
+            up4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_up_4.png")));
             down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_down_1.png")));
             down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_down_2.png")));
+            down3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_down_3.png")));
+            down4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_down_4.png")));
             left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_left_1.png")));
             left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_left_2.png")));
+            left3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_left_3.png")));
+            left4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_left_4.png")));
             right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_right_1.png")));
             right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_right_2.png")));
+            right3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_right_3.png")));
+            right4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/player_right_4.png")));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -44,30 +52,38 @@ public class player extends entity {
     }
 
     public void update() { // update the player
-        if (keyH.up) { // if the up key is pressed
-            direction = "up"; // set the direction to up
-            y -= speed; // move the player up
-        }
-        if (keyH.down) { // if the down key is pressed
-            direction = "down"; // set the direction to down
-            y += speed; // move the player down
-        }
-        if (keyH.left) { // if the left key is pressed
-            direction = "left"; // set the direction to left
-            x -= speed; // move the player left
-        }
-        if (keyH.right) { // if the right key is pressed
-            direction = "right"; // set the direction to right
-            x += speed; // move the player right
-        }
 
-        spriteCounter++; // increment the sprite counter
-        if (spriteCounter > 12){
-            if (spriteNum == 1){
-                spriteNum = 2;
+        if (keyH.up == true || keyH.down == true || keyH.left == true || keyH.right == true) {
+
+            if (keyH.up) { // if the up key is pressed
+                direction = "up"; // set the direction to up
+                y -= speed; // move the player up
             }
-            else if (spriteNum == 2){
-                spriteNum = 1;
+            if (keyH.down) { // if the down key is pressed
+                direction = "down"; // set the direction to down
+                y += speed; // move the player down
+            }
+            if (keyH.left) { // if the left key is pressed
+                direction = "left"; // set the direction to left
+                x -= speed; // move the player left
+            }
+            if (keyH.right) { // if the right key is pressed
+                direction = "right"; // set the direction to right
+                x += speed; // move the player right
+            }
+
+            spriteCounter++; // increment the sprite counter
+            if (spriteCounter > 15) {
+                spriteCounter = 0;
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                } else if (spriteNum == 2) {
+                    spriteNum = 3;
+                } else if (spriteNum == 3) {
+                    spriteNum = 4;
+                } else if (spriteNum == 4) {
+                    spriteNum = 1;
+                }
             }
         }
     }
@@ -75,37 +91,49 @@ public class player extends entity {
     public void draw(Graphics g2) { // draw the player
         BufferedImage image = null; // image to draw
 
-        switch (direction){
+        switch (direction) {
             case "up":
                 if (spriteNum == 1) {
                     image = up1;
-                }
-                if (spriteNum == 2) {
+                } else if (spriteNum == 2) {
                     image = up2;
+                } else if (spriteNum == 3) {
+                    image = up3;
+                } else if (spriteNum == 4) {
+                    image = up4;
                 }
                 break;
             case "down":
                 if (spriteNum == 1) {
                     image = down1;
-                }
-                if (spriteNum == 2) {
+                } else if (spriteNum == 2) {
                     image = down2;
+                } else if (spriteNum == 3) {
+                    image = down3;
+                } else if (spriteNum == 4) {
+                    image = down4;
                 }
                 break;
             case "left":
                 if (spriteNum == 1) {
                     image = left1;
-                }
-                if (spriteNum == 2) {
+                } else if (spriteNum == 2) {
                     image = left2;
+                } else if (spriteNum == 3) {
+                    image = left3;
+                } else if (spriteNum == 4) {
+                    image = left4;
                 }
                 break;
             case "right":
                 if (spriteNum == 1) {
                     image = right1;
-                }
-                if (spriteNum == 2) {
+                } else if (spriteNum == 2) {
                     image = right2;
+                } else if (spriteNum == 3) {
+                    image = right3;
+                } else if (spriteNum == 4) {
+                    image = right4;
                 }
                 break;
         }
